4302e3ea76b5eeba13256d347decf607cbcf0b4a
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-05-10 23:24:59 +0100

    feat(menu):Creacion de la clase menu con un metodo estatico para printear el menu

---
+++ src/net/salesianos/menu/Menu.java
@@ -0,0 +1,15 @@
+package net.salesianos.menu;
+
+public class Menu {
+    public static void mostrarMenu() {
+
+        System.out.println("\nSeleccione una opción:" +
+                "\n1. Añadir palabra" +
+                "\n2. Eliminar palabra" +
+                "\n3. Verificar si existe una palabra" +
+                "\n4. Mostrar iniciales disponibles" +
+                "\n5. Ver palabras por inicial" +
+                "\n6. Cerrar programa");
+    }
+
+}
