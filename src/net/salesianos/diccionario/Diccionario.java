85817c704779bb5d6292c4255a5c538aab90dae0
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-05-10 23:20:16 +0100

    feat(Diccionario):Creacion ultimos dos metodos para conseguir iniciales disponibles, y conseguir palabras por inicial

--- src/net/salesianos/diccionario/Diccionario.java
+++ src/net/salesianos/diccionario/Diccionario.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class Diccionario {
-     private Map<Character, Set<String>> diccionario;
+    private Map<Character, Set<String>> diccionario;
 
     public Diccionario() {
         diccionario = new HashMap<>();
@@ -18,17 +18,27 @@ public class Diccionario {
         diccionario.putIfAbsent(inicial, new HashSet<>());
         diccionario.get(inicial).add(palabra);
     }
+
     public void eliminarPalabra(String palabra) {
         char inicial = Character.toLowerCase(palabra.charAt(0));
         palabra = palabra.trim().toLowerCase();
         diccionario.get(inicial).remove(palabra);
 
     }
-    
+
     public boolean contienePalabra(String palabra) {
         char inicial = Character.toLowerCase(palabra.charAt(0));
         palabra = palabra.trim().toLowerCase();
         return diccionario.get(inicial) != null && diccionario.get(inicial).contains(palabra);
+    }
+
+    public Set<Character> conseguirInicialesDisponibles() {
+        return diccionario.keySet();
+    }
+
+    public Set<String> conseguirPalabrasPorInicial(char inicial) {
+        inicial = Character.toLowerCase(inicial);
+        return diccionario.getOrDefault(inicial, null);
     }
 
 }
