f4db181a52b5455202785cf16a71a5c00a049d4d
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-05-10 23:18:22 +0100

    feat(Diccionario):Creacion de la clase diccionario con su atributo, constructor y primer metodo para agregar palabras

---
+++ src/net/salesianos/diccionario/Diccionario.java
@@ -0,0 +1,21 @@
+package net.salesianos.diccionario;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Diccionario {
+     private Map<Character, Set<String>> diccionario;
+
+    public Diccionario() {
+        diccionario = new HashMap<>();
+    }
+
+    public void agregarPalabra(String palabra) {
+        char inicial = Character.toLowerCase(palabra.charAt(0));
+        palabra = palabra.trim().toLowerCase();
+        diccionario.putIfAbsent(inicial, new HashSet<>());
+        diccionario.get(inicial).add(palabra);
+    }
+}
